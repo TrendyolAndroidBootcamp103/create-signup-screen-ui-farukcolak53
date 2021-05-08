38dd36957b8cbe4b9a61a031cbda0357b9693b5e
Faruk Colak <faruk_colak53@hotmail.com>
2021-05-08 16:24:00 +0300

    Fix validation bugs.

--- app/src/main/java/school/cactus/succulentshop/SignUpEmailValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/SignUpEmailValidator.kt
@@ -3,9 +3,9 @@ package school.cactus.succulentshop
 class SignUpEmailValidator: Validator {
     override fun validate(field: String) = when {
         field.isEmpty() -> R.string.signup_email_error
-        !field.contains(""".&@""".toRegex()) -> R.string.signup_invalid_email_error
-        field.length < 5 -> R.string.signup_invalid_email_error
-        field.length > 50 -> R.string.signup_invalid_email_error
+        !(field.contains('.') and field.contains('@'))  -> R.string.signup_invalid_email_error
+        field.length <= 5 -> R.string.signup_invalid_email_error
+        field.length >= 50 -> R.string.signup_invalid_email_error
         else -> null
     }
 }
